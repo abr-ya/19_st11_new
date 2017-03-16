1114871a158beb88c2dfa6452d0f7548a0df3c1c
abr-ya <gamer05@list.ru>
2017-03-16 14:53:02 +0300

    plus_link

--- init.sh
+++ init.sh
@@ -1,7 +1,3 @@
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
-sudo rm /etc/nginx/sites-available/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
-sudo /etc/init.d/nginx restart
 gunicorn -c /home/box/web/etc/hello.py hello:app &
